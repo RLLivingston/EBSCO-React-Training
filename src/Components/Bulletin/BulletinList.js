f9c569f8d57058ffc53c516f9bc57886c17bdca3
Robert <robertleelivingston@gmail.com>
2019-11-04 07:44:08 -0600

    Styling in place

--- src/Components/Bulletin/BulletinList.js
+++ src/Components/Bulletin/BulletinList.js
@@ -1,7 +1,49 @@
 import React from "react";
 import Bulletin from "./Bulletin";
+import avatarImage from "../../images/avatars/daniel.jpg";
+import productImage from "../../images/products/image-aqua.png";
+
+const LocalData = [
+    {
+      "id": "b9277c30-8989-4f11-a005-437fc7854247",
+      "description": "Actors to help with your Medival parties!",
+      "title": "Fort Knights",
+      "url": null,
+      "votes": 222,
+      "avatarUrl": "./images/avatars/daniel.jpg",
+      "bulletinImageUrl": "./images/products/image-aqua.png"
+    },
+    {
+      "id": "2f616e97-f848-4893-b36c-b747760d0bd1",
+      "description": "Turning your car from blah to bling!",
+      "title": "Drop Top Customs",
+      "url": null,
+      "votes": 19,
+      "avatarUrl": "../../images/avatars/steve.jpg",
+      "bulletinImageUrl": "../../images/products/image-steel.png"
+    },
+    {
+      "id": "6161c1b6-d530-4b6b-9cd7-cbbedb4b1c3a",
+      "description": "Fashionable headwear for the conspiracy theorist in you!",
+      "title": "Tin Foil Hats",
+      "url": null,
+      "votes": 13,
+      "avatarUrl": "../../images/avatars/jenny.jpg",
+      "bulletinImageUrl": "../../images/products/image-rose.png"
+    },
+    {
+      "id": "9c7205d7-93f9-4bb4-bc32-c951fdc886ea",
+      "description": "The best food in the kingdom, brought to your door!",
+      "title": "Gilded Rose Catering",
+      "url": null,
+      "votes": 21,
+      "avatarUrl": "../images/avatars/kristy.png",
+      "bulletinImageUrl": "../images/products/image-yellow.png"
+    }
+  ];
 
 export default class BulletinList extends React.Component {
+
     state = {
         error: null,
         isLoaded: false,
@@ -92,14 +134,20 @@ export default class BulletinList extends React.Component {
                 description={item.description}
                 url={item.url}
                 votes={item.votes}
-                avatarUrl={item.avatarUrl}
-                bulletinImageUrl={item.bulletinImageUrl}
+                avatarUrl={avatarImage}
+                bulletinImageUrl={productImage}
                 onVote={this.handleBulletinUpvote}
               />
             );
           });
         }
+
+        
     
-        return <div className="ui unstackable items">{bulletinComponents}</div>;
+        return <div class="main ui text container">
+        <div id="content">
+            <div className="ui unstackable items">{bulletinComponents}</div>
+        </div>
+    </div>;
       }
 }
